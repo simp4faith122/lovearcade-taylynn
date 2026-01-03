d227d481fb7a02c24a963da597859e1b8b998b88
simp4faith122 <kenCar2on.72@gmail.com>
2026-01-03 09:50:28 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  const btn=document.getElementById('playBtn');
-  btn.addEventListener('click',()=>{
-    alert('Play functionality not implemented yet — welcome to Love Arcade — Taylynn!');
-  });
-});
